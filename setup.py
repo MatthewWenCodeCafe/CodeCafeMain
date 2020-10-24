acbc862a2a4d876edb63d8963771d240058036fe
matthewen <mattwen2018@gmail.com>
2020-10-24 01:39:56 -0400

    Testing Setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name='code_cafe',
+    version="0.0.0",
+    install_requires=requirements,
+    author="Matthew Wen",
+    author_email="mattwen2018@gmail.com",
+    description="Code For Code Cafe",
+    packages=setuptools.find_packages(),
+    scripts=["bin/collect-photos"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ]
+)
